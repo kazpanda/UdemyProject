cc34fe70451ac5d1ae7b45ad2815addd4ea257fb
kazpanda <kazpanda@gmail.com>
2020-04-25 22:54:39 +0900

    Nuget追加

---
+++ 30.CSProject/11.テスト駆動/ClassLibrary2/ClassLibrary2/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    /// <summary>
+    /// サンプルクラス
+    /// </summary>
+    public class Class1
+    {
+        /// <summary>
+        /// メソッド１
+        /// 処理が一つの場合
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int Add1(int a,int b) {
+            // 処理1
+            return a + b;
+        }
+
+        /// <summary>
+        /// メソッド２
+        /// 処理が複数の場合
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static int Add2(int a , int b,int c) {
+
+            // 処理1
+            if(a == 1) {
+                return b + c;
+            }
+            // 処理2
+            return a + b + c;
+        }
+    }
+}
