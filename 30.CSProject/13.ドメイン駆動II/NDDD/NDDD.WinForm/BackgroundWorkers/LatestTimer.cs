d9fac4db6ef79b3c26206f81b81587cf775ac394
panda kaz <kaz_panda@hotmail.com>
2020-05-31 17:55:46 +0900

    timer追加

---
+++ 30.CSProject/13.ドメイン駆動II/NDDD/NDDD.WinForm/BackgroundWorkers/LatestTimer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NDDD.WinForm.BackgroundWorkers {
+    internal static class LatestTimer {
+
+        private static Timer _timer;
+        private static bool _isWork=false;
+
+        static LatestTimer() {
+            _timer = new Timer(Callback);
+        }
+
+        internal static void Start() {
+            _timer.Change(10000, 10000);
+        }
+
+        internal static void Stop() {
+            _timer.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+
+        private static void Callback(object o) {
+            if (_isWork) {
+                return;
+            }
+
+            try {
+                _isWork = true;
+                // timerの処理
+            }
+            finally{
+                _isWork = false;
+            }
+        }
+
+    }
+}
